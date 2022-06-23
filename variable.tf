80d07de88bf843afef79e9edd862e7a72683a3ef
hamdan-siddiqi <inamsiddiqi95@gmail.com>
2022-06-23 11:40:36 +0530

    moving files

---
+++ variable.tf
@@ -0,0 +1,26 @@
+variable "subscription_id" {
+    type = string 
+    default = "e9d7db19-8a35-4c42-b637-876bffbd6394"
+  
+}
+
+variable "client_id" {
+    type = string 
+    default = "deb3726e-9a2f-46d4-831a-a818ed5ccd81"
+  
+}
+
+variable "client_secret" {
+    type = string 
+    default = "StH8Q~3WXhDAYs7e7U3Qhf86XRxGUUbkGw9jMaoZ"
+  
+}
+
+
+variable "tenant_id" {
+    type = string 
+    default = "7a339603-4648-4c91-b19b-2092e551c652"
+  
+}
+
+
